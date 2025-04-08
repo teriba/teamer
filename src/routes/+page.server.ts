bd902c1dab2f0fb1ba85dcb27c6212d28376ab83
Erik Bäckström <erik.backstrom@gmail.com>
2025-04-09 01:40:18 +0200

    Deploy to netlify

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,98 @@
 import type { PageServerLoad } from './$types';
+import type { Actions } from './$types';
+
+function shuffle(array: string[]) {
+	let currentIndex = array.length;
+
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
+		// Pick a remaining element...
+		let randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
+}
+
+const xp = {
+	a: ['Milo', 'Elton', 'Hannes', 'Charlie B'],
+	b: ['Maximilian', 'Charlie G', 'Lucas', 'Kevin', 'Ludvig'],
+	c: ['Tova', 'Lova', 'Julia', 'Oscar', 'Tyra', 'Saga', 'Miranda', 'Iris', 'Kelly', 'Miranda']
+};
+
+export const actions = {
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+		const players = data.getAll('players');
+
+		let one: { xp: number; players: string[] } = {
+			xp: 0,
+			players: []
+		};
+
+		let two: { xp: number; players: string[] } = {
+			xp: 0,
+			players: []
+		};
+
+		let a: string[] = [];
+		let b: string[] = [];
+		let c: string[] = [];
+
+		xp.a.forEach((player) => {
+			if (players.includes(player)) {
+				a.push(player);
+			}
+		});
+		xp.b.forEach((player) => {
+			if (players.includes(player)) {
+				b.push(player);
+			}
+		});
+		xp.c.forEach((player) => {
+			if (players.includes(player)) {
+				c.push(player);
+			}
+		});
+
+		shuffle(a);
+		shuffle(b);
+		shuffle(c);
+
+		a.forEach((p) => {
+			if (one.xp > two.xp) {
+				two.players.push(p);
+				two.xp += 4;
+			} else {
+				one.players.push(p);
+				one.xp += 4;
+			}
+		});
+
+		b.forEach((p) => {
+			if (one.xp > two.xp) {
+				two.players.push(p);
+				two.xp += 2;
+			} else {
+				one.players.push(p);
+				one.xp += 2;
+			}
+		});
+
+		c.forEach((p) => {
+			if (one.xp > two.xp) {
+				two.players.push(p);
+				two.xp += 1;
+			} else {
+				one.players.push(p);
+				one.xp += 1;
+			}
+		});
+
+		return { players, teamOne: one.players, teamTwo: two.players };
+	}
+} satisfies Actions;
 
 export const load: PageServerLoad = async () => {
 	const players = [
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 
-	let { data }: PageProps = $props();
+	let { data, form }: PageProps = $props();
+	let selectedPlayers: string[] = (form?.players as string[]) || [];
 </script>
 
 <div class="p-5">
@@ -9,10 +10,41 @@
 </div>
 
 <div class="flex-col items-center space-x-2">
-	{#each data.players as player}
-		<div class="flex items-center space-x-2">
-			<input type="checkbox" id={player} />
-			<label for={player}>{player}</label>
-		</div>
-	{/each}
+	<form method="POST">
+		{#each data.players as player}
+			<div class="flex items-center space-x-2">
+				<input
+					type="checkbox"
+					value={player}
+					name="players"
+					bind:group={selectedPlayers}
+					id={`player-${player}`}
+				/>
+				<label for={`player-${player}`}>{player}</label>
+			</div>
+		{/each}
+		<button class="m-5 flex w-52 justify-center justify-self-center rounded-2xl bg-green-500 p-4"
+			>Skapa lag</button
+		>
+	</form>
+	<div>
+		{#if form}
+			<h2 class="font-bold">Lag 1</h2>
+			<ul>
+				{#each form.teamOne as team}
+					<li>{team}</li>
+				{/each}
+			</ul>
+
+			<hr class="my-5" />
+			<h2 class="font-bold">Lag 2</h2>
+			<ul>
+				{#each form.teamTwo as team}
+					<li>{team}</li>
+				{/each}
+			</ul>
+		{:else}
+			<p>Inga lag skapade än.</p>
+		{/if}
+	</div>
 </div>
